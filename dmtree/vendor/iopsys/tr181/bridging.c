4364f3b529b27b9b9a51a022d975ea2c2129a349
Amin Ben Romdhane <amin.benromdhane@iopsys.eu>
2023-02-10 14:15:34 +0100

    Network update: deprecate vendor extension 'dspc2pbit'

--- dmtree/vendor/iopsys/tr181/bridging.c
+++ dmtree/vendor/iopsys/tr181/bridging.c
@@ -39,33 +39,6 @@ static int set_BridgingBridgePort_Egress_PriorityRegeneration(char *refparam, st
 	return 0;
 }
 
-static int get_BridgingBridgePort_DSCP_Eth_Priority_Map(char *refparam, struct dmctx *ctx, void *data, char *instance, char **value)
-{
-	bridging_get_priority_list("dscp2pbit", data, value);
-	return 0;
-}
-
-static int set_BridgingBridgePort_DSCP_Eth_Priority_Map(char *refparam, struct dmctx *ctx, void *data, char *instance, char *value, int action)
-{
-	char *type = NULL;
-
-	switch (action) {
-		case VALUECHECK:
-			if (dm_validate_unsignedInt_list(value, 1, 64, -1, RANGE_ARGS{{"0","7"}}, 1))
-				return FAULT_9007;
-
-			dmuci_get_value_by_section_string(((struct bridge_port_args *)data)->bridge_port_sec, "type", &type);
-			if (DM_STRLEN(type) == 0)
-				return FAULT_9007;
-
-			return 0;
-		case VALUESET:
-			bridging_set_priority_list("dscp2pbit", data, value);
-			return 0;
-	}
-	return 0;
-}
-
 static int get_BridgingBridgeVLAN_TVID(char *refparam, struct dmctx *ctx, void *data, char *instance, char **value)
 {
 	bridging_get_vlan_tvid("tvid", data, value);
@@ -92,7 +65,6 @@ static int set_BridgingBridgeVLAN_TVID(char *refparam, struct dmctx *ctx, void *
 DMLEAF tIOPSYS_BridgingBridgePortParams[] = {
 /* PARAM, permission, type, getvalue, setvalue, bbfdm_type*/
 {BBF_VENDOR_PREFIX"EgressPriorityRegeneration", &DMWRITE, DMT_STRING, get_BridgingBridgePort_Egress_PriorityRegeneration, set_BridgingBridgePort_Egress_PriorityRegeneration, BBFDM_BOTH},
-{BBF_VENDOR_PREFIX"DSCPEthernetPriorityMapping", &DMWRITE, DMT_STRING, get_BridgingBridgePort_DSCP_Eth_Priority_Map, set_BridgingBridgePort_DSCP_Eth_Priority_Map, BBFDM_BOTH},
 {0}
 };
 
