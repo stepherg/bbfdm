783d750dc0012028449de4d1647a9ca728726df9
Vivek Kumar Dutta <vivek.dutta@iopsys.eu>
2023-07-18 09:43:22 +0530

    Updated file headers

--- bbfdmd/src/add_delete.c
+++ bbfdmd/src/add_delete.c
@@ -1,24 +1,12 @@
 /*
  * add_delete.c: Add/Delete handler for bbfdmd
  *
- * Copyright (C) 2023 iopsys Software Solutions AB. All rights reserved.
+ * Copyright (C) 2023 IOPSYS Software Solutions AB. All rights reserved.
  *
  * Author: Vivek Dutta <vivek.dutta@iopsys.eu>
  * Author: Amin Ben Romdhane <amin.benromdhane@iopsys.eu>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
+ * See LICENSE file for license related information.
  */
 
 #include "common.h"
