f36f29046cc00486eeeff78f6859a58cb157c333
Amin Ben Romdhane <amin.benromdhane@iopsys.eu>
2024-02-23 16:07:25 +0100

    Pipeline: Added test scripts

---
+++ gitlab-ci/functional-test-python.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "$0 preparation script"
+pwd
+
+source ./gitlab-ci/shared.sh
+
+echo "Starting services..."
+cp ./gitlab-ci/bbfdm_services.conf /etc/supervisor/conf.d/
+
+supervisorctl reread
+supervisorctl update
+sleep 10
+
+supervisorctl status all
+exec_cmd ubus wait_for bbfdm
+
+# Test ubus bbfdm 'notify_event' method
+./test/python-test-cases/python/validate_ubus_notify_event_method.py test/python-test-cases/json/ubus_notify_event_method.json
+
+# Test 'bbfdm.event' event callback
+./test/python-test-cases/python/validate_event_callback.py test/python-test-cases/json/event_callback.json
+
+# Test 'bbfdm.AddObj' & 'bbfdm.DelObj' event
+./test/python-test-cases/python/validate_add_del_event.py test/python-test-cases/json/add_del_event.json
+
+# Test ubus bbfdm 'get' method
+./test/python-test-cases/python/validate_ubus_get_method.py test/python-test-cases/json/ubus_get_method.json
+
+# Test ubus bbfdm 'instances' method
+./test/python-test-cases/python/validate_ubus_schema_method.py test/python-test-cases/json/ubus_schema_method.json
+
+# Test ubus bbfdm 'add' method
+./test/python-test-cases/python/validate_ubus_add_method.py test/python-test-cases/json/ubus_add_method.json
+
+# Test ubus bbfdm 'del' method
+./test/python-test-cases/python/validate_ubus_del_method.py test/python-test-cases/json/ubus_del_method.json
+
+# Test ubus bbfdm 'schema' method
+./test/python-test-cases/python/validate_ubus_schema_method.py test/python-test-cases/json/ubus_schema_method.json
+
+supervisorctl stop all
+supervisorctl status
+
+echo "Functional Test :: PASS"
---
+++ test/python-test-cases/python/validate_ubus_instances_method.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import sys
+import ubus
+import json
+
+class TestArguments:
+    """
+    Class to hold test arguments.
+    """
+    def __init__(self, description, service, input, expected_error, output):
+        self.description = description
+        self.service = service
+        self.input = input
+        self.expected_error = expected_error
+        self.output = output
+
+def run_test(args):
+    """
+    Run a single test.
+    """
+
+    print("Running: " + args.description)
+
+    out = ubus.call(args.service, "instances", args.input)
+    
+    if isinstance(out, list) and out:
+        out = out[0]
+    else:
+        print("FAIL: " + args.description)
+        return
+        
+    res_len = len(out["results"])
+    fault = out["results"][0]["fault"] if res_len and "fault" in out["results"][0] else 0
+        
+    if fault != args.expected_error:
+        print("FAIL: " + args.description)
+        return
+        
+    if args.expected_error == 0 and res_len == 0:
+        print("FAIL: " + args.description)
+        return
+    
+    # Check if output matches expected output
+    if args.output != {}:
+        if out == args.output:
+            print("PASS: " + args.description)
+        else:
+            print("FAIL: " + args.description)
+    else:
+        print("PASS: " + args.description)
+
+if __name__ == "__main__":
+    # Check for correct command line arguments
+    if len(sys.argv) != 2:
+        print("Usage: {} <test_arguments_json>".format(sys.argv[0]))
+        sys.exit(1)
+
+    test_arguments_file = sys.argv[1]
+
+    try:
+        with open(test_arguments_file, 'r') as f:
+            test_arguments_data = json.load(f)
+            service_name = test_arguments_data.get("object", "bbfdm")
+            args_list = [TestArguments(**test_case, service=service_name) for test_case in test_arguments_data["instances"]]
+    except FileNotFoundError:
+        print("File not found:", test_arguments_file)
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        print("Error parsing JSON:", e)
+        sys.exit(1)
+    
+    ubus.connect()
+
+    # Run tests for each set of arguments
+    for args in args_list:
+        run_test(args)
+        
+    ubus.disconnect()
