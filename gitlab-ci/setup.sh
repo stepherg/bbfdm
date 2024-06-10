a132a1915afe2c77c615df82efb7fbceb98bb54a
Vivek Kumar Dutta <vivek.dutta@iopsys.eu>
2024-06-10 12:43:27 +0530

    Setup rpcd utilities in pipeline

--- gitlab-ci/setup.sh
+++ gitlab-ci/setup.sh
@@ -5,6 +5,9 @@ echo "# Preparation script ..."
 # link '/bin/sh' to bash instead of dash
 ln -sf bash /bin/sh
 
+echo "Installing bbfdm rpcd utilities"
+cp utilities/files/usr/libexec/rpcd/* /usr/libexec/rpcd/
+
 cp -r ./test/files/etc/* /etc/
 cp -r ./test/files/usr/* /usr/
 cp -r ./test/files/var/* /var/
